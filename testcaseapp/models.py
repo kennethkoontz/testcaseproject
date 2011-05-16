5f0df04fb9f285a20498afc1c04b68d37a985abf
kenneth koontz <kenneth koontz>
2011-05-15 22:59:43 -0700

    add test suite

--- testcaseapp/models.py
+++ testcaseapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class TestSuite(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+
+
