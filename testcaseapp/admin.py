94284fa0a0949ca6bfbb376d0741e618abe18f72
kenneth koontz <kenneth koontz>
2011-05-16 20:26:55 -0700

    update admin.py

--- testcaseapp/admin.py
+++ testcaseapp/admin.py
@@ -1,6 +1,8 @@
 from testcaseapp.models import TestSuite, Milestone, TestCase
 from django.contrib import admin
 
-admin.site.register(TestSuite, TestCase, Milestone)
+admin.site.register(TestSuite)
+admin.site.register(Milestone)
+admin.site.register(TestCase)
 
 
